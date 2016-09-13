c09b282a122ccbedc1336e471a05c7ececb625cf
Takenori Tanaka <jazzsaxplayer02@gmail.com>
2016-09-13 17:27:49 +0900

    画像追加

---
+++ app/webroot/theme/nada-icons/Pages/comibura.php
@@ -0,0 +1,39 @@
+<!-- BaserPageTagBegin -->
+<?php $this->BcBaser->setTitle('荻川コミぶら散歩') ?>
+<?php $this->BcBaser->setDescription('') ?>
+<?php $this->BcBaser->setPageEditLink(29) ?>
+<!-- BaserPageTagEnd -->
+
+<h1>コミぶら散歩</h1>
+
+<p>&nbsp;</p>
+
+<p><a href="/ogicomi/files/uploads/P6180599.jpg" rel="colorbox" title="P6180599.JPG"><img align="" alt="P6180599.JPG" src="/ogicomi/files/uploads/P6180599.jpg" /></a></p>
+
+<p>&nbsp;</p>
+
+<p><a href="/ogicomi/files/uploads/P6180600.jpg" rel="colorbox" title="P6180600.JPG"><img align="" alt="P6180600.JPG" src="/ogicomi/files/uploads/P6180600.jpg" /></a></p>
+
+<p>&nbsp;</p>
+
+<p><a href="/ogicomi/files/uploads/P6180602.jpg" rel="colorbox" title="P6180602.JPG"><img align="" alt="P6180602.JPG" src="/ogicomi/files/uploads/P6180602.jpg" /></a></p>
+
+<p>&nbsp;</p>
+
+<p><a href="/ogicomi/files/uploads/P6180613.jpg" rel="colorbox" title="P6180613.JPG"><img align="" alt="P6180613.JPG" src="/ogicomi/files/uploads/P6180613.jpg" /></a></p>
+
+<p>&nbsp;</p>
+
+<p><a href="/ogicomi/files/uploads/P6180614.jpg" rel="colorbox" title="P6180614.JPG"><img align="" alt="P6180614.JPG" src="/ogicomi/files/uploads/P6180614.jpg" /></a></p>
+
+<p>&nbsp;</p>
+
+<p><a href="/ogicomi/files/uploads/P6180619.jpg" rel="colorbox" title="P6180619.JPG"><img align="" alt="P6180619.JPG" src="/ogicomi/files/uploads/P6180619.jpg" /></a></p>
+
+<p>&nbsp;</p>
+
+<p><a href="/ogicomi/files/uploads/P6180631.jpg" rel="colorbox" title="P6180631.JPG"><img align="" alt="P6180631.JPG" src="/ogicomi/files/uploads/P6180631.jpg" /></a></p>
+
+<p>&nbsp;</p>
+
+<p><a href="/ogicomi/files/uploads/P6180636.jpg" rel="colorbox" title="P6180636.JPG"><img align="" alt="P6180636.JPG" src="/ogicomi/files/uploads/P6180636.jpg" /></a></p>
---
+++ app/webroot/theme/nada-icons/Pages/ogikawa_news.php
@@ -0,0 +1,11 @@
+<!-- BaserPageTagBegin -->
+<?php $this->BcBaser->setTitle('地域のニュース') ?>
+<?php $this->BcBaser->setDescription('') ?>
+<?php $this->BcBaser->setPageEditLink(28) ?>
+<!-- BaserPageTagEnd -->
+
+<h1>地域のニュース</h1>
+
+<p><a href="/ogicomi/files/uploads/075037__midium.jpg" rel="colorbox" title="075037.jpg"><img align="" alt="075037.jpg" src="/ogicomi/files/uploads/075037__small.jpg" /></a></p>
+
+<p>このページでは、荻川地区の話題を発信します。</p>
